0564e7100ae5ba4a9b56bc2139fd88afebee26e0
hz-uiw <kwonmc5.11@gmail.com>
2025-01-06 22:01:51 +0900

    유저 조회/추가

--- src/main/java/com/korit/servlet_study/servlet/UserServlet.java
+++ src/main/java/com/korit/servlet_study/servlet/UserServlet.java
@@ -12,10 +12,10 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @WebServlet("/user")
 public class UserServlet extends HttpServlet {
-    @Override
     public void init(ServletConfig config) throws ServletException {
         List<User> users = new ArrayList<>();
         users.add(new User("aaa", "1111", "aaaaaa", "aaa@gmail.com"));
@@ -27,28 +27,43 @@ public class UserServlet extends HttpServlet {
         config.getServletContext().setAttribute("users", users);
     }
 
-    @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String searchValue = req.getParameter("searchValue");
         ServletContext servletContext = req.getServletContext();
         List<User> users = (List<User>) servletContext.getAttribute("users");
-        req.setAttribute("users", users);
+
+        if(searchValue != null) {
+            if(searchValue.isBlank()) {
+                req.setAttribute("users", users.stream()
+                        .filter(user -> user.getUsername().contains(searchValue))
+                        .collect(Collectors.toList()));
+            }
+        }
         req.getRequestDispatcher("/WEB-INF/user.jsp").forward(req, resp);
     }
 
-    @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String username = req.getParameter("username");
-        String password = req.getParameter("password");
-        String name = req.getParameter("name");
-        String email = req.getParameter("email");
+        User user = User.builder()
+                .username(req.getParameter("username"))
+                .password(req.getParameter("password"))
+                .name(req.getParameter("name"))
+                .email(req.getParameter("email"))
+                .build();
 
+        ServletContext servletContext = req.getServletContext();
+        List<User> users = (List<User>) servletContext.getAttribute("users");
+        if(users.stream().filter(u -> u.getUsername().equals(user.getUsername())).collect(Collectors.toList()).size() > 0) {
+            resp.setContentType("text/html");
+            resp.getWriter().println(
+                    "<script>" +
+                    "alert('이미 존재하는 사용자 이름입니다.');" +
+                    "history.back();" +
+                    "</script>"
+            );
+            return;
+        }
+        users.add(user);
 
-        resp.setContentType("text/html");
-        resp.getWriter().println(
-                "<script>" +
-                        "alert(\"등록이 완료되었습니다.\");" +
-                        "location.href='https://localhost:8080/servlet_study_war/user';" +
-                "</script>"
-        );
+        resp.sendRedirect("http://localhost:8080/servlet_study_war/user");
     }
 }
