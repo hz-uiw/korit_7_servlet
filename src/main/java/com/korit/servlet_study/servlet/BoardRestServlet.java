062770b9f0551ab2392e1ad85c606af2af4e7996
hz-uiw <kwonmc5.11@gmail.com>
2025-01-21 12:46:02 +0900

    Board

--- src/main/java/com/korit/servlet_study/servlet/BoardRestServlet.java
+++ src/main/java/com/korit/servlet_study/servlet/BoardRestServlet.java
@@ -1,5 +1,10 @@
 package com.korit.servlet_study.servlet;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.korit.servlet_study.dto.InsertBoardDto;
+import com.korit.servlet_study.entity.Board;
+import com.korit.servlet_study.service.BoardService;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -10,14 +15,25 @@ import java.io.IOException;
 
 @WebServlet("/api/board")
 public class BoardRestServlet extends HttpServlet {
+
+    public BoardService boardService;
+
+    public BoardRestServlet() { boardService = BoardService.getInstance(); }
+
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        StringBuilder stringBuilder = new StringBuilder(); // 싱글 스레드 용
+//        StringBuffer stringBuffer = new StringBuffer(); // 멀티 스레드 용
         // 받아온 axios.post()가 json형태여서 getReader() 메서드 사용
         try (BufferedReader bufferedReader = request.getReader()) {
             String line;
             while ((line = bufferedReader.readLine()) != null) {
-                System.out.println(line);
+                stringBuilder.append(line);
             }
         }
+        ObjectMapper objectMapper = new ObjectMapper();
+        InsertBoardDto insertBoardDto = objectMapper.readValue(stringBuilder.toString(), InsertBoardDto.class);
+
+        boardService.insertBoard(insertBoardDto);
 
     }
 }
