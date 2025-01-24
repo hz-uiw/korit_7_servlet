35781821bb241177f02ccbfae09204a88c05708b
hz-uiw <kwonmc5.11@gmail.com>
2025-01-24 12:31:08 +0900

    Jwt

--- src/main/java/com/korit/servlet_study/filter/AuthenticationFilter.java
+++ src/main/java/com/korit/servlet_study/filter/AuthenticationFilter.java
@@ -1,41 +1,85 @@
 package com.korit.servlet_study.filter;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.korit.servlet_study.dao.UserDao;
 import com.korit.servlet_study.dto.ResponseDto;
+import com.korit.servlet_study.entity.User;
+import com.korit.servlet_study.security.annotation.JwtValid;
 import com.korit.servlet_study.security.jwt.JwtProvider;
+import io.jsonwebtoken.Claims;
 
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.lang.reflect.Method;
 
 @WebFilter("*")
 public class AuthenticationFilter implements Filter {
     private JwtProvider jwtProvider;
+    private UserDao userDao;
 
     public AuthenticationFilter() {
         jwtProvider = JwtProvider.getInstance();
+        userDao = UserDao.getInstance();
     }
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         HttpServletRequest request = (HttpServletRequest) servletRequest;
         HttpServletResponse response = (HttpServletResponse) servletResponse;
-        String path = request.getServletPath();
-        String method = request.getMethod();
 
-        ObjectMapper objectMapper = new ObjectMapper();
-        ResponseDto<?> responseDto = null;
+        try {
+            if (isJwtTokenValid(request)) {
+                // 토큰 없으면 403으로 응답
+                String bearerToken = request.getHeader("Authorization");
+                if(bearerToken == null) {
+                    setUnAuthenticatedResponse(response);
+                    return;
+                }
 
+                Claims claims = jwtProvider.parseToken(bearerToken);
+                if(claims == null) {
+                    setUnAuthenticatedResponse(response);
+                    return;
+                }
 
-        String bearerToken = request.getHeader("Authorization");
-        if(bearerToken == null) {
-            responseDto = ResponseDto.forbidden("검증할 수 없는 Access Token입니다.");
-            response.setStatus(responseDto.getStatus());
-            response.setContentType("application/json");
-            response.getWriter().println(objectMapper.writeValueAsString(responseDto));
-            return;
+                int userId = Integer.parseInt(claims.get("userId").toString());
+                User foundUser = userDao.findByID(userId);
+                if(foundUser == null) {
+                    setUnAuthenticatedResponse(response);
+                    return;
+                }
+            }
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
         }
-        jwtProvider.parseToken(bearerToken);
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+    private boolean isJwtTokenValid(HttpServletRequest request) throws ClassNotFoundException {
+        String method = request.getMethod();
+        String servletPath = request.getHttpServletMapping().getServletName();
+
+        Class<?> servletClass = Class.forName(servletPath);
+        Method foundMethod = getMappedMethod(servletClass, method);
+        return foundMethod != null;
+    }
+
+    private Method getMappedMethod(Class<?> clazz, String methodName) {
+        for(Method method: clazz.getDeclaredMethods()) {
+            if(method.getName().toLowerCase().endsWith(methodName.toLowerCase()) && method.isAnnotationPresent(JwtValid.class)) {
+                return method;
+            }
+        }
+        return null;
+    }
+
+    private void setUnAuthenticatedResponse(HttpServletResponse response) throws IOException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        ResponseDto<String> responseDto = ResponseDto.forbidden("검증할 수 없는 Access Token입니다.");
+        response.setStatus(responseDto.getStatus());
+        response.setContentType("application/json");
+        response.getWriter().println(objectMapper.writeValueAsString(responseDto));
     }
 }
